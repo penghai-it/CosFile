df18c1a2910882a120af7dcf43c0c6177a05c909
彭海 <564990676@qq.com>
2023-01-30 11:11:40 +0800

    add 漏提交

---
+++ cosFileTest/src/main/java/it/ph/com/cosfiletest/service/UserCartService.java
@@ -0,0 +1,22 @@
+package it.ph.com.cosfiletest.service;
+
+import it.ph.com.cosfiletest.mode.Cart;
+
+import java.util.Map;
+
+/**
+ * @author PH
+ * @时间： 2022/11/28
+ * @描述：普通用户购物车处理
+ */
+public interface UserCartService {
+    /**
+     * @param userId 用户id
+     * @param items  商品信息 <商品id,商品数量>
+     * @return: it.ph.com.cosfiletest.mode.Cart
+     * @创建者: PH
+     * @时间: 2022/11/28
+     * @描述:普通用户购物车处理过程
+     **/
+    Cart normalUserCart(long userId, Map<Long, Integer> items);
+}
---
+++ cosFileTest/src/main/java/it/ph/com/cosfiletest/test/Streamtest2.java
@@ -0,0 +1,33 @@
+package it.ph.com.cosfiletest.test;
+
+import it.ph.com.cosfiletest.mode.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author PH
+ * @时间： 2022/11/28
+ * @描述：；流__练习
+ */
+public class Streamtest2 {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(6, 7, 3, 8, 1, 2, 9);
+        list.stream().filter(x -> x > 7).forEach(System.out::println);
+
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Tom", 8900, 23, "male", "New York"));
+        personList.add(new Person("Jack", 7000, 25, "male", "Washington"));
+        personList.add(new Person("Lily", 7800, 21, "female", "Washington"));
+        personList.add(new Person("Anni", 8200, 24, "female", "New York"));
+        personList.add(new Person("Owen", 9500, 25, "male", "New York"));
+        personList.add(new Person("Alisa", 7900, 26, "female", "New York"));
+
+        List<String> fiterList = personList.stream().filter(x -> x.getSalary() > 8000).map(Person::getName).collect(Collectors.toList());
+        System.out.print("薪资高于8000美元的员工：" + fiterList);
+    }
+}
